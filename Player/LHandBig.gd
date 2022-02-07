0bea47eed41b9ba086c8975d34bdd587c42dde70
295cade1 <cade@cadekelly.com>
2022-02-06 21:53:34 -0500

    Hands now properly interact with rigidbodies
    ---Turns out PinJoints are good for something---

--- Player/LHandBig.gd
+++ Player/LHandBig.gd
@@ -18,7 +18,8 @@ onready var target_hand = get_node(target_hand_path)
 var player_movement_vector = Vector3(0,0,0)
 
 var position_locked = false
-var pinjoint
+var pinjoint = null
+var connected_object = null
 
 var played_thud = false
 
@@ -27,6 +28,8 @@ func _ready():
 	self.set_as_toplevel(true)
 
 func _integrate_forces(state):
+	if(!position_locked):
+		button_pressed(2)
 	move_hand(state)
 
 func get_desired_hand_pos():
@@ -45,85 +48,90 @@ func move_hand(state):
 	var mismatch = clamp(distance/MAXMISMATCH,0,MAXMISMATCH)
 	var target_velocity = target_direction*lerp(0,MAXARMSPEED,mismatch)
 
-	
+	##LINEAR MOVEMENT##
+	#Resets the player movement vector
+	player_movement_vector = Vector3(0,0,0)
+	#Gets our velocity relative to the player
+	state.linear_velocity = state.linear_velocity - player.linear_velocity
+	#Gets the change in velocity that we need to make
+	var velocity_difference = target_velocity - state.linear_velocity
+	#Makes sure that velocity is within our max force
+	var total_velocity = velocity_difference.normalized() * min(velocity_difference.length(),1) * MAXFORCE
+
+	#Big for loop to get the distance to the nearest object in the direction we are trying to move
+	var min_phys_length = 0
+	var collision_shapes = self.get_children()
 	if(position_locked):
-		
-		self.player_movement_vector = -target_velocity
+		collision_shapes.append(connected_object.get_children())
+
+	for child in collision_shapes:
+		if(child is CollisionShape):
+			if(child.disabled): continue
+			var phys_cast = PhysicsShapeQueryParameters.new()
+			phys_cast.shape_rid = (child.shape.get_rid())
+			phys_cast.transform = child.global_transform
+			phys_cast.exclude = [get_rid(), player.get_rid()]
+
+			if(position_locked):
+				phys_cast.exclude.append(connected_object.get_rid())
+
+			var new_phys_length = state.get_space_state().cast_motion(phys_cast,(total_velocity / self.mass) * state.step)[0]
+			if(min_phys_length < new_phys_length):
+				min_phys_length = new_phys_length
+	
+	#Sets the velocity of the hand based on how far we can move - if the area is clear, just move the hand
+	var hand_velocity = total_velocity * min_phys_length
+	#Sets the player velocity based on how obstructed the area we are trying to move in is
+	player_movement_vector = -total_velocity * (1 - min_phys_length)
+	#Actually add the hand velocity to the hand
+	state.add_central_force(hand_velocity)
+	#Finally make our velocity global again
+	state.linear_velocity = state.linear_velocity + player.linear_velocity
+
+	##ROTATION##
+
+	state.angular_velocity = state.angular_velocity - player.angular_velocity
+
+	var target_rotation = target_hand.global_transform.basis
+	var current_rotation = self.global_transform.basis
+
+	var rotation = Vector3(0,0,0)
+
+	#Gets our target rotation directions
+	rotation.x = _get_angle_to_from_axis(current_rotation.z, target_rotation.z, Vector3(1,0,0)) * PI
+	rotation.y = _get_angle_to_from_axis(current_rotation.x, target_rotation.x, Vector3(0,1,0)) * PI
+	rotation.z = _get_angle_to_from_axis(current_rotation.y, target_rotation.y, Vector3(0,0,1)) * PI
+
+	velocity_difference = rotation - state.angular_velocity
+
+	total_velocity = velocity_difference.normalized() * min(velocity_difference.length(),1) * ROTSPEED
+
+	#Actually set the angular velocity so we rotate towards the target rotation
+	state.add_torque(total_velocity)
+	state.angular_velocity = state.angular_velocity + player.angular_velocity
+
+	## SOUND FX ##
+	if(!played_thud):
+		if(min_phys_length < 0.25):
+			play_thud()
+			played_thud = true
 	else:
-		##LINEAR MOVEMENT##
-		#Resets the player movement vector
-		player_movement_vector = Vector3(0,0,0)
-		#Gets our velocity relative to the player
-		state.linear_velocity = state.linear_velocity - player.linear_velocity
-		#Gets the change in velocity that we need to make
-		var velocity_difference = target_velocity - state.linear_velocity
-		#Makes sure that velocity is within our max force
-		var total_velocity = velocity_difference.normalized() * min(velocity_difference.length(),1) * MAXFORCE
-
-		#Big for loop to get the distance to the nearest object in the direction we are trying to move
-		var min_phys_length = 0
-		for child in self.get_children():
-			if(child is CollisionShape):
-				if(child.disabled): continue
-				var phys_cast = PhysicsShapeQueryParameters.new()
-				phys_cast.shape_rid = (child.shape.get_rid())
-				phys_cast.transform = child.global_transform
-				phys_cast.exclude = [get_rid(), player.get_rid()]
-				var new_phys_length = state.get_space_state().cast_motion(phys_cast,(total_velocity / self.mass) * state.step)[0]
-				if(min_phys_length < new_phys_length):
-					min_phys_length = new_phys_length
-		
-		#Sets the velocity of the hand based on how far we can move - if the area is clear, just move the hand
-		var hand_velocity = total_velocity * min_phys_length
-		#Sets the player velocity based on how obstructed the area we are trying to move in is
-		player_movement_vector = -total_velocity * (1 - min_phys_length)
-		#Actually add the hand velocity to the hand
-		state.add_central_force(hand_velocity)
-		#Finally make our velocity global again
-		state.linear_velocity = state.linear_velocity + player.linear_velocity
-
-		##ROTATION##
-
-		state.angular_velocity = state.angular_velocity - player.angular_velocity
-
-		var target_rotation = target_hand.global_transform.basis
-		var current_rotation = self.global_transform.basis
-
-		var rotation = Vector3(0,0,0)
-
-		#Gets our target rotation directions
-		rotation.x = _get_angle_to_from_axis(current_rotation.z, target_rotation.z, Vector3(1,0,0)) * PI
-		rotation.y = _get_angle_to_from_axis(current_rotation.x, target_rotation.x, Vector3(0,1,0)) * PI
-		rotation.z = _get_angle_to_from_axis(current_rotation.y, target_rotation.y, Vector3(0,0,1)) * PI
-
-		velocity_difference = rotation - state.angular_velocity
-
-		total_velocity = velocity_difference.normalized() * min(velocity_difference.length(),1) * ROTSPEED
-
-		#Actually set the angular velocity so we rotate towards the target rotation
-		state.add_torque(total_velocity)
-		state.angular_velocity = state.angular_velocity + player.angular_velocity
-
-		## SOUND FX ##
-		if(!played_thud):
-			if(min_phys_length < 0.25):
-				play_thud()
-				played_thud = true
-		else:
-			if(min_phys_length == 1):
-				played_thud = false
-		
+		if(min_phys_length == 1):
+			played_thud = false
+	
 
 func try_lock():
 	if(raycast.is_colliding()):
+		print("colliding")
 		pinjoint = PinJoint.new()
 		pinjoint.global_transform.origin = self.global_transform.origin
 		pinjoint.set("nodes/node_b", self.get_path())
 		if(raycast.get_collider() is RigidBody):
-			pinjoint.set("nodes/node_a", raycast.get_collider())
+			pinjoint.set("nodes/node_a", raycast.get_collider().get_path())
 			pinjoint.global_transform.origin = raycast.get_collider().global_transform.origin
 		get_tree().root.add_child(pinjoint)
 		position_locked = true
+		connected_object = raycast.get_collider()
 		change_hand_collision(2)
 	else:
 		change_hand_collision(1)
